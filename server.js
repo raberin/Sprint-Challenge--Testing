1c9113e61b6338bf0ab00f7fa5f40efb16dfe936
raberin <rmvaberin@gmail.com>
2019-06-30 19:37:23 -0700

    First endpoint working and tested

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 
 const server = express();
-const db = require("./data/db.js");
+
+const db = require("./data/games/gamesModel.js");
+
 server.use(express.json());
 
-server.get("/games", (req, res) => {
-  null;
+server.get("/games", async (req, res) => {
+  try {
+    const games = await db.get();
+    if (games.length < 1) {
+      res.status(200).json([]);
+    } else {
+      res.status(200).json(games);
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "We ran into an error retrieving the games" });
+  }
 });
 
 server.post("/games", (req, res) => {
--- server.spec.js
+++ server.spec.js
@@ -1,11 +1,50 @@
 const request = require("supertest");
 const server = require("./server.js");
-const db = require("./data/db.js");
+const games = require("./data/games/gamesModel.js");
+const db = require("./data/dbConfig.js");
 
 describe("server.js", () => {
   describe("GET /games", () => {
+    beforeEach(async () => {
+      // this function executes and clears out the table before each test
+      await db("games").truncate();
+    });
+
     it("should return a 200 code after get call", async () => {
       const res = await request(server).get("/games");
+      expect(res.status).toBe(200);
+    });
+
+    it("should return a JSON", async () => {
+      const res = await request(server).get("/games");
+      expect(res.type).toBe("application/json");
+    });
+
+    it("should return an empty arr when empty", async () => {
+      const res = await request(server).get("/games");
+      expect(res.body).toEqual([]);
+    });
+  });
+
+  describe("POST /games", () => {
+    beforeEach(async () => {
+      // this function executes and clears out the table before each test
+      await db("games").truncate();
+    });
+
+    it("should return a 201 code after insertion", async () => {
+      const res = await request(server).post("/games");
+      expect(res.status).toBe(200);
+    });
+
+    it("should return a JSON", async () => {
+      const res = await request(server).get("/games");
+      expect(res.type).toBe("application/json");
+    });
+
+    it("should return an empty arr when empty", async () => {
+      const res = await request(server).get("/games");
+      expect(res.body).toEqual([]);
     });
   });
 });
